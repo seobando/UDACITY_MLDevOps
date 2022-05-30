1ac48a82c446f3a6aa134e3e8a2cd7cdfbc347f9
Sebastian OM <sebastianobandomorales@gmail.com>
2022-05-30 16:15:25 -0500

    Preprocessing

--- Project 2 - Build an ML Pipeline for Short-term Rental Prices in NYC/src/basic_cleaning/run.py
+++ Project 2 - Build an ML Pipeline for Short-term Rental Prices in NYC/src/basic_cleaning/run.py
@@ -6,6 +6,8 @@ import argparse
 import logging
 import wandb
 
+import pandas as pd
+import wandb
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
@@ -20,37 +22,100 @@ def go(args):
     # particular version of the artifact
     # artifact_local_path = run.use_artifact(args.input_artifact).file()
 
-    ######################
-    # YOUR CODE HERE     #
-    ######################
+    # Download input artifact.
+    logger.info("Downloading artifact")
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
+    
+    # Load the data
+    logger.info("Loading the data")
+    df = pd.read_parquet(artifact_local_path)
+
+    # Preprocessing
+    logger.info("Start pre-processing")
+    # Drop outliers
+    min_price = args.min_price
+    max_price = args.max_price
+    idx = df['price'].between(min_price, max_price)
+    df = df[idx].copy()
+    # Convert last_review to datetime
+    df['last_review'] = pd.to_datetime(df['last_review'])
+
+    # Prepare file to be saved
+    filename = args.output_artifact
+    df.to_csv(filename)
+
+    artifact = wandb.Artifact(
+        name=args.artifact_name,
+        type=args.artifact_type,
+        description=args.artifact_description
+    )
+
+    artifact.add_file(filename)
+
+    logger.info("Logging artifact")
+    run.log_artifact(artifact)
+    artifact.wait()
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="This steps cleans the data")
 
-
+    # Parameter 1
     parser.add_argument(
-        "--parameter1", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--input_artifact", 
+        type=str,
+        help="Input dataset",
         required=True
     )
 
+    # Parameter 2
     parser.add_argument(
-        "--parameter2", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--output_artifact", 
+        type=str,
+        help="Output dataset",
         required=True
     )
 
+    # Parameter 3
     parser.add_argument(
-        "--parameter3", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--artifact_name", 
+        type=str,
+        help="wandb artifact name",
         required=True
     )
 
+    # Parameter 4
+    parser.add_argument(
+        "--artifact_type", 
+        type=str,
+        help="wandb artifact type",
+        required=True
+    )
+
+    # Parameter 5
+    parser.add_argument(
+        "--artifact_description", 
+        type=str,
+        help="wandb artifact description",
+        required=True
+    )
+
+    # Parameter 6
+    parser.add_argument(
+        "--min_price", 
+        type=float,
+        help="lower limit tolerant for outlier",
+        required=True
+    )
+
+    # Parameter 7
+    parser.add_argument(
+        "--max_price", 
+        type=float,
+        help="superior limit tolerant for outlier",
+        required=True
+    )
 
     args = parser.parse_args()
 
