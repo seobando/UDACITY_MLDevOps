dc115bf75af61f56feed67d732cc479e503a6935
Sebastian OM <sebastianobandomorales@gmail.com>
2022-06-02 19:59:56 -0500

    basic cleaning bug fix

--- Project-2-Short-term-Rental-Prices-in-NYC/src/basic_cleaning/run.py
+++ Project-2-Short-term-Rental-Prices-in-NYC/src/basic_cleaning/run.py
@@ -37,6 +37,9 @@ def go(args):
     df['last_review'] = pd.to_datetime(df['last_review'])
 
     # Prepare file to be saved
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     filename = args.output_artifact
     df.to_csv(filename)
 
